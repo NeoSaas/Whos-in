ef6ad576aa990daaa1246bdea648b8f5c4cf5f8f
Aaron Perel <a.a.ron.perel9@gmail.com>
2025-04-01 00:57:37 -0400

    Update firebaseconfig.ts

--- firebaseconfig.ts
+++ firebaseconfig.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 const env = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: env.VITE_APIKEY,
-  authDomain: env.VITE_AUTHDOMAIN,
-  projectId: env.VITE_PROJECTID,
-  storageBucket: env.VITE_STORAGEBUCKET,
-  messagingSenderId: env.VITE_MESSAGINGSENDERID,
-  appId: env.VITE_APPID,
+  apiKey: `${env.VITE_APIKEY}`,
+  authDomain: `${env.VITE_AUTHDOMAIN}`,
+  projectId: `${env.VITE_PROJECTID}`,
+  storageBucket: `${env.VITE_STORAGEBUCKET}`,
+  messagingSenderId: `${env.VITE_MESSAGINGSENDERID}`,
+  appId: `${env.VITE_APPID}`,
 };
 
 // Initialize Firebase
